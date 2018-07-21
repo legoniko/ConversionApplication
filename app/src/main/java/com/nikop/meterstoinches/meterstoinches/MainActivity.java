47a50dc9f19642d082ad3996d2011904c212a35b
legoniko <nikopappas5@gmail.com>
2018-07-20 22:29:57 -0400

    This is a test commit from my new Desktop.

--- app/src/main/java/com/nikop/meterstoinches/meterstoinches/MainActivity.java
+++ app/src/main/java/com/nikop/meterstoinches/meterstoinches/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         resultTextView.setVisibility(View.VISIBLE);
     }
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
